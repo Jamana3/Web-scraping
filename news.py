c5634c81783b863200f6b68ae7e1b61fa6608e55
Jamana3 <e3jamna141592@gmail.com>
2025-01-27 01:09:13 +0530

    Update news.py

--- news.py
+++ news.py
@@ -1,7 +1,3 @@
-## In this file we collected news from newsapi.org containing keywords war,election and disaster, either in their
-#title or description. We also calculated average sentiment score for each day for each keyword and article count.
-# stored the fetched data in pandas dataframe and use them in our main.py file.
-
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
@@ -50,23 +46,15 @@ class NewsSentimentAnalyzer:
                 print(f"Failed to fetch data for keyword '{keyword}'")
 
     def calculate_sentiment(self):
-        #Calculate sentiment scores for titles and descriptions in all_data DataFrame."
-        # Define a helper function for calculating sentiment
         def get_sentiment(text):
             return self.analyzer.polarity_scores(text)['compound'] if pd.notnull(text) else 0
-
-        # Calculate sentiment scores
         self.all_data['title_sentiment'] = self.all_data['title'].apply(get_sentiment)
         self.all_data['description_sentiment'] = self.all_data['description'].apply(get_sentiment)
         self.all_data['average_sentiment'] = self.all_data[['title_sentiment', 'description_sentiment']].mean(axis=1)
 
     def get_daily_summary(self):
-        #Aggregate data to get daily counts and average sentiment scores by keyword.
-        # Convert 'date' to datetime format and sort by date
         self.all_data['date'] = pd.to_datetime(self.all_data['date'])
         self.all_data = self.all_data.sort_values('date').reset_index(drop=True)
-
-        # Aggregate daily sentiment scores and keyword counts
         daily_sentiment = self.all_data.groupby(['date', 'keyword'])['average_sentiment'].mean().unstack(fill_value=0)
         keyword_counts = self.all_data.groupby(['date', 'keyword']).size().unstack(fill_value=0)
 
